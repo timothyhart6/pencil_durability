29d0e130ddabcd07dc9d24be91c013c55d3804d3
Timothy Hart <timothyhart@MacBook-Air.local>
2021-03-13 09:33:58 -0500

    Added eraser_spec.rb. Added pending examples for #erase.

---
+++ spec/specs/eraser_spec.rb
@@ -0,0 +1,13 @@
+describe 'Eraser' do
+  context '#erase' do
+    it 'only erases the specified text'
+    it 'erases the most recently written text'
+    it 'can erase text next to leading white space'
+    it 'can erase text next to trailing white space'
+    it 'can erase text between other characters'
+    it 'can erase the same text multiple times'
+    it 'is case sensative'
+    it 'will not erase more characters than eraser durability'
+    it 'will not erase text that does not match'
+  end
+end
